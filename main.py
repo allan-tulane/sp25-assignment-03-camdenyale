0914bef31343a16ef00c126c64c85ec86e243924
camdenyale <cyale@tulane.edu>
2025-04-03 14:38:22 -0500

    Update main.py
    Updates

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev-
 def MED(S, T):
     # TO DO - modify to account for insertions, deletions and substitutions
     if (S == ""):
-        return(len(T))
+        return (len(T))
     elif (T == ""):
-        return(len(S))
+        return (len(S))
     else:
         if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+            return (MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return (1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
 def fast_MED(S, T, MED={}):
-    if (S, T) in MED:
+        if (S, T) in MED:
         return MED[(S, T)]
 
     if T == "":
@@ -37,5 +37,27 @@ def fast_MED(S, T, MED={}):
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
 
+    elif T == "":
+        MED[(S, T)] = (S, "-" * len(S))
+    elif S == "":
+        MED[(S, T)] = ("-" * len(T), T)
+
+    elif S[0] == T[0]:
+        align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        MED[(S, T)] = (S[0] + align_S, T[0] + align_T)
+    else:
+        ins_S, ins_T = fast_align_MED(S, T[1:], MED)
+        del_S, del_T = fast_align_MED(S[1:], T, MED)
+
+        ins_C = 1 + len(ins_S)
+        del_C = 1 + len(del_S)
+
+        if ins_C <= del_C:
+            MED[(S, T)] = ("-" + ins_S, T[0] + ins_T)
+        else:
+            MED[(S, T)] = (S[0] + del_S, "-" + del_T)
+
+    return MED[(S, T)]
